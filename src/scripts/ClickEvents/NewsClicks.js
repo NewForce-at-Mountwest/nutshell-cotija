b118be37afd685d79e2f196bd569e4e62ec41827
JACOB <JACOBPRY@AOl.com>
2019-09-13 10:04:12 -0400

    Testing Alex's PR

--- src/scripts/ClickEvents/NewsClicks.js
+++ src/scripts/ClickEvents/NewsClicks.js
@@ -6,6 +6,12 @@ const testButton =document.querySelector("#test-submit-button");
 
 testButton.addEventListener("click", function(){
     console.log("Youve clicked the Test Button")
-    
+
+    const newsTitleInput = document.querySelector("#news-title").value;
+
+    const newsUrlInput = document.querySelector("#news-url").value;
+
+    const newsSumInput = document.querySelector("#news-sum").value;
+
 
 })
