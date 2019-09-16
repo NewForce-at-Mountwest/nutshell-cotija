f8dac5dbc2e9382fc422bd6b82617fe52d5b6cab
newforce <seaverstanley1408@gmail.com>
2019-09-16 15:38:02 -0400

    new pull request

--- src/scripts/ClickEvents/LoginClickEvents.js
+++ src/scripts/ClickEvents/LoginClickEvents.js
@@ -2,7 +2,7 @@ import apiManager from "../apiManagers/UsersLoginAPI.js"
 
 const Login ={
 loginClickEvent:()=>{
-// ------- CLICK EVENT FOR LOGIN----------------//
+
 document.querySelector("#login-button").addEventListener("click", () => {
   // Get the username and password values from the form
   console.log("the button works!")
@@ -11,13 +11,14 @@ document.querySelector("#login-button").addEventListener("click", () => {
   // Use the username to go to the datbaase and get that user's information
   apiManager.getOneUserByUsername(usernameValue).then(user => {
     // User is going to be an array no matter what, so we'll have to delve into the array to get the user's data
-    debugger
     console.log("This is user", user)
     // Compare the user's password from the db to the information they entered
     console.log(user[0].UserPassword, passwordValue);
+    debugger
     if (user[0].UserPassword === passwordValue) {
       // If the passwords match, store the id in local storage
       localStorage.setItem("userId", user[0].id);
+      localStorage.setItem("activeUser", user.activeUser[0].id)
     } else {
       // Error handling would go here
       console.log("Incorrect password!");
