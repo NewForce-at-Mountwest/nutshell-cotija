c30eff04e239364d0e7c5ce2db39d7d1b3f1fe8a
Your Name <lukemiller4779@gmail.com>
2019-09-17 14:22:08 -0400

    mvpgit add .git add .

--- src/scripts/ClickEvents/LoginClickEvents.js
+++ src/scripts/ClickEvents/LoginClickEvents.js
@@ -1,4 +1,7 @@
 import apiManager from "../apiManagers/UsersLoginAPI.js"
+import sendChatButtonFunction from "./ChatClicks.js"
+import renderChat from "../domPrinter/ChatPrinter.js"
+import apiChat from "../apiManagers/Chatapi.js"
 const Login ={
 loginClickEvent:()=>{
 // ------- CLICK EVENT FOR LOGIN----------------//
@@ -6,30 +9,35 @@ document.querySelector("#login-button").addEventListener("click", () => {
 
   // Get the username and password values from the form
   console.log("the button works!")
-  const usernameValue = document.querySelector("#username-input").value;
-  const passwordValue = document.querySelector("#password-input").value;
+  const usernameValue = document.querySelector("#username-input");
+  const passwordValue = document.querySelector("#password-input");
+
   // Use the username to go to the datbaase and get that user's information
-  apiManager.getOneUserByUsername(usernameValue).then(user => {
+  apiManager.getOneUserByUsername(usernameValue.value).then(user => {
     // User is going to be an array no matter what, so we'll have to delve into the array to get the user's data
     debugger
     console.log("This is user", user)
     // Compare the user's password from the db to the information they entered
-    console.log(user[0].UserPassword, passwordValue);
-    if (user[0].UserPassword === passwordValue) {
+    console.log(user[0].UserPassword, passwordValue.value);
+    if (user[0].UserPassword === passwordValue.value) {
       // If the passwords match, store the id in local storage
       localStorage.setItem("userId", user[0].id);
-      apiChat.getAllMessages()
-      .then(parsedMessages=>{
-          renderChat.buildChatCard(parsedMessages)})
-      // Prints chat input and button
       document.querySelector("#chatroom").innerHTML+= `<fieldset>
       <label for="chat">Chatroom</label>
       <br>
       <input type="text" name="chat" id="Chatinput">
       </fieldset>
       <button id="Sendmsg" type="submit">Send</button>`
+
+      apiChat.getAllMessages()
+      .then(parsedMessages=>{
+          renderChat.buildChatCard(parsedMessages)})
+      // Prints chat input and button
+
       // calls function to send a new message to json and reprint
       sendChatButtonFunction()
+      usernameValue.value = ""
+      passwordValue.value = ""
     } else {
       // Error handling would go here
       console.log("Incorrect password!");
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,5 @@
 import registerClickEvents from "./ClickEvents/UsersRegisterClicks.js"
 import Login from "./ClickEvents/LoginClickEvents.js"
-import sendChatButtonFunction from "./ClickEvents/ChatClicks.js"
-import renderChat from "./domPrinter/ChatPrinter.js"
-import apiChat from "./apiManagers/Chatapi.js"
 registerClickEvents.createAccount()
 Login.loginClickEvent()
 
-apiChat.getAllMessages()
-.then(parsedMessages=>{
-    renderChat.buildChatCard(parsedMessages)})
-// Prints chat input and button
-document.querySelector("#chatroom").innerHTML+= `<fieldset>
-<label for="chat">Chatroom</label>
-<br>
-<input type="text" name="chat" id="Chatinput">
-</fieldset>
-<button id="Sendmsg" type="submit">Send</button>`
-// calls function to send a new message to json and reprint
-sendChatButtonFunction()
